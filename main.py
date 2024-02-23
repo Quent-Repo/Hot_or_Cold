7fb6527a2df83642f25f3d60e9515078d2b6ee57
owner <grrkekisbest@gmail.com>
2024-02-23 00:55:57 -0500

    Background is now an image

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ app = Flask(__name__)
 
 Info= requests.get("https://api.open-meteo.com/v1/gfs?latitude=40.7312&longitude=-74.2735&hourly=temperature_2m&temperature_unit=fahrenheit&wind_speed_unit=mph")
 
-print(Info.json()["hourly"]["time"][0],Info.json()["hourly"]["temperature_2m"][0])
-Time=Info.json()["hourly"]["time"][0]
-Temp=Info.json()["hourly"]["temperature_2m"][0]
+print(Info.json()["hourly"]["time"][2],Info.json()["hourly"]["temperature_2m"][2])
+Time,Temp=Info.json()["hourly"]["time"][2],Info.json()["hourly"]["temperature_2m"][2]
 
 @app.route("/")
 def home():
