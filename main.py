f44ad11171b36043dc134a998e44dff16ab80819
owner <grrkekisbest@gmail.com>
2024-02-26 21:53:33 -0500

    added in js to html to get time in hoiours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, render_template
+from flask import Flask, render_template, requests
 app = Flask(__name__)
 
 Info= requests.get("https://api.open-meteo.com/v1/gfs?latitude=40.7312&longitude=-74.2735&hourly=temperature_2m&temperature_unit=fahrenheit&wind_speed_unit=mph")
